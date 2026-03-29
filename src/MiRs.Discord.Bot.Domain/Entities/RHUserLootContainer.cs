1e27b6b1added6fe83c5ecae705fe210801d4397
fizz_ <fizz_@DESKTOP-0HKGMNR>
2026-03-29 22:39:48 +0100

    attach json property name

--- src/MiRs.Discord.Bot.Domain/Entities/RHUserLootContainer.cs
+++ src/MiRs.Discord.Bot.Domain/Entities/RHUserLootContainer.cs
@@ -1,7 +1,10 @@
-﻿namespace MiRs.Discord.Bot.Domain.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace MiRs.Discord.Bot.Domain.Entities
 {
     public class RHUserLootContainer
     {
+        [JsonPropertyName("Loots")]
         public List<RHUserLoot> UserLoot { get; set; }
     }
 }
